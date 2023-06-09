5b1ab4a7a9a8a8e861277eb2dbb7fcc3ace1e84a
Gleb <glebshaevvv@mail.ru>
2023-06-09 18:25:25 +0300

    header anchor complete

--- src/scripts/anchor.js
+++ src/scripts/anchor.js
@@ -3,16 +3,13 @@ import { removeBurger, removeActive, removeOverlay } from "scripts/header-menu.j
 $(function() {
   var hash = document.location.hash;
   if (hash.length>1 && $(hash).length){ 
-    console.log($(hash).offset().top)
       $('html, body').animate({
           scrollTop: $(hash).offset().top-100
-      }, 100);
+      }, 700);
   }
 });
 
-
 document.addEventListener('DOMContentLoaded', () => {
-  
   
   const allLinks = document.querySelectorAll('a')
   
@@ -20,11 +17,10 @@ document.addEventListener('DOMContentLoaded', () => {
     btn.addEventListener('click', () => {
       $(function() {
         var hash = document.location.hash;
-        console.log($(hash).offset().top)
         if (hash.length>1 && $(hash).length){ 
             $('html, body').animate({
                 scrollTop: $(hash).offset().top-100
-            }, 0);
+            }, 500);
         }
       });
       removeBurger()
