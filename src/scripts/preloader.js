d1c1b2d4384295ba357e692084771d4ba4c003c2
Сергей Викторович Князев <113102209+Knyazew36@users.noreply.github.com>
2023-06-19 16:00:07 +0300

    test

--- src/scripts/preloader.js
+++ src/scripts/preloader.js
@@ -3,7 +3,7 @@ import * as parallax from './parallax'
 
 document.body.classList.add("body--hidden");
 
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('load', () => {
   document.body.classList.remove("body--notransition");
   const preloader = document.querySelector(".preloader")
 
@@ -12,25 +12,25 @@ document.addEventListener('DOMContentLoaded', () => {
     const DELAY = 1800
     preloader.classList.add('preloader--hidden')
 
-    // setTimeout(() => {
-    //   document.body.classList.remove("body--hidden");
-    //   window.scrollTo(0, 0);
-    //   video.setAttribute("autoplay", "");
-    //   video.load();
+    setTimeout(() => {
+      document.body.classList.remove("body--hidden");
+      window.scrollTo(0, 0);
+      // video.setAttribute("autoplay", "");
+      // video.load();
 
-    //   video.addEventListener('canplaythrough', () => {
-    //     preloader.classList.add('preloader--hide')
-    //     AOS.init({
-    //       once: true,
-    //       offset: 100,
-    //       duration: 1000,
-    //     });
-    //     parallax.start()
-    //     const ecologyVideo = document.querySelector('[data-ecology-video]')
-    //     ecologyVideo.setAttribute("autoplay", "");
-    //     ecologyVideo.load()
-    //   })
-    // }, DELAY);
+      preloader.classList.add('preloader--hide')
+      // video.addEventListener('canplaythrough', () => {
+      //   AOS.init({
+      //     once: true,
+      //     offset: 100,
+      //     duration: 1000,
+      //   });
+      //   parallax.start()
+      // const ecologyVideo = document.querySelector('[data-ecology-video]')
+      // ecologyVideo.setAttribute("autoplay", "");
+      // ecologyVideo.load()
+      // })
+    }, DELAY);
   } else {
     document.body.classList.remove("body--hidden");
 
