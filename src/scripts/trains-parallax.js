125d3b6671a94cc77ee5d5442689fadf164a9f89
Gleb <glebshaevvv@mail.ru>
2023-06-21 19:33:04 +0300

    update

--- src/scripts/trains-parallax.js
+++ src/scripts/trains-parallax.js
@@ -1,16 +1,21 @@
+import { mediaQuery } from "./mediaQueries"
+
 document.addEventListener('DOMContentLoaded', () => {
   const parallaxContainer = document.querySelector('[data-parallax-container]')
   const parallaxImg = document.querySelector('[data-parallax-img]');
 
+  if(parallaxContainer){
 
-  window.addEventListener('scroll', () => {
-    let windowScroll = window.scrollY;
+    window.addEventListener('scroll', () => {
+      let windowScroll = window.scrollY;
+  
+      const parallaxStart = parallaxContainer.offsetTop;
+  
+      if(windowScroll > parallaxStart - 900){
+        let scroll = ((windowScroll - parallaxStart) + 75)/2 + 'px';
+        parallaxImg.style.bottom = scroll;
+      }
+    })
+  }
 
-    const parallaxStart = parallaxContainer.offsetTop;
-
-    if(windowScroll > parallaxStart - 900){
-      console.log('done')
-      parallaxImg.style.bottom = (windowScroll - parallaxStart) + 75 + 'px';
-    }
-  })
 })
