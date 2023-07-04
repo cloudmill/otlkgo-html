6230d514e2a6b88fff4ee88fc8922158e42ca54e
Сергей Викторович Князев <113102209+Knyazew36@users.noreply.github.com>
2023-07-04 11:06:01 +0300

    3

--- src/scripts/fancybox.js
+++ src/scripts/fancybox.js
@@ -5,28 +5,10 @@ export const fancybox = () => {
   const buttons = document.querySelectorAll('.fancy-button');
 
   buttons.forEach((button) => {
-    const img = button.getAttribute('fancy-modal');
-    const fancyModal = document.querySelector(`[data-fancy-img="${img}"]`);
-
-    Fancybox.bind(fancyModal, {
-      closeButton: true,
-      Thumbs: false,
-      Toolbar: true,
-      // on: {
-      //   loading: (fancybox) => {
-      //     fancybox.container.classList.add('my-custom-container');
-      //     fancybox.container.style.opacity = 0;
-      //     setTimeout(() => {
-      //       fancybox.container.style.opacity = 1;
-      //     }, 350);
-      //   },
-      // },
-    });
-
     button.addEventListener('click', () => {
+      let href = button.getAttribute('data-fancy')
       Fancybox.show([{
-        src: fancyModal.getAttribute('src'),
-        caption: fancyModal.getAttribute('alt')
+        src: href,
       }], {
         closeButton: true,
         Thumbs: false,
