2539ad6d512fd641936dd5583d0f88669e0887c4
Сергей Викторович Князев <113102209+Knyazew36@users.noreply.github.com>
2023-07-10 11:42:25 +0300

    no message

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -5,7 +5,7 @@ export const modal = () => {
   const modal = document.querySelector('.modal ');
   const overlay = document.querySelector('.modal__overlay');
   const closeBtn = document.querySelector('.modal__close');
-
+  const exitBtn = document.querySelector('.modal-submit__button')
   if (!modalBtn) return;
 
   modalBtn.addEventListener('click', () => {
@@ -13,14 +13,28 @@ export const modal = () => {
     modal.classList.add('--active');
   });
 
+  exitBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    closeModal();
+  })
+  modal.addEventListener('submit', (e) => {
+    e.preventDefault()
+    modal.classList.add('--submit')
+  })
   function closeModal() {
     enablePageScroll();
     modal.classList.remove('--active');
+    setTimeout(() => {
+      modal.classList.remove('--submit');
+
+    }, 100)
   }
 
   overlay.addEventListener('click', () => {
     closeModal();
   });
+
   closeBtn.addEventListener('click', () => {
     closeModal();
   });
