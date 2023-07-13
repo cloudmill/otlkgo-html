896daaa4141fd23dac7fb61f2887d4631586845d
Gleb <glebshaevvv@mail.ru>
2023-07-13 11:37:09 +0300

    update

--- src/scripts/anchor.js
+++ src/scripts/anchor.js
@@ -24,9 +24,50 @@ setTimeout(() => {
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  const allLinks = document.querySelectorAll('a');
+  // const allLinks = document.querySelectorAll('a');
 
-  allLinks.forEach(btn => {
+  const allLinkTitles = document.querySelectorAll('.header-sub__item-title')
+  const allLinkSubTitles = document.querySelectorAll('.header-sub__item-subsection')
+
+  // allLinks.forEach(btn => {
+  //   btn.addEventListener('click', () => {
+  //     setTimeout(() => {
+  //       $(function () {
+  //         var hash = document.location.hash;
+  //         if (hash.length > 1 && $(hash).length) {
+  //           $('html, body').animate({
+  //             scrollTop: $(hash).offset().top - offsetScroll
+  //           }, 500);
+  //         }
+  //       });
+  //       removeBurger();
+  //       removeActive();
+  //       removeOverlay();
+  //     }, 100)
+      
+  //   });
+  // });
+
+  allLinkTitles.forEach(btn => {
+    btn.addEventListener('click', () => {
+      setTimeout(() => {
+        $(function () {
+          var hash = document.location.hash;
+          if (hash.length > 1 && $(hash).length) {
+            $('html, body').animate({
+              scrollTop: $(hash).offset().top - offsetScroll
+            }, 500);
+          }
+        });
+        removeBurger();
+        removeActive();
+        removeOverlay();
+      }, 100)
+      
+    });
+  });
+
+  allLinkSubTitles.forEach(btn => {
     btn.addEventListener('click', () => {
       setTimeout(() => {
         $(function () {
