3d5ac75f9393bba233a6e9c86a52d9bd524633b5
Сергей Викторович Князев <113102209+Knyazew36@users.noreply.github.com>
2023-07-17 13:28:36 +0300

    1

--- src/scripts/print-button.js
+++ src/scripts/print-button.js
@@ -1,3 +1,30 @@
+
+
+
+window.addEventListener('beforeprint', () => {
+  isSafari()
+  const body = document.querySelector('body')
+
+  if (isSafari()) {
+    body.classList.add('print-ios')
+  } else {
+    body.classList.add('print')
+  }
+
+})
+
+function isSafari() {
+  return /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+}
+
+
+
+
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const printBtn = document.querySelector('[data-print-button]')
   if (printBtn) {
