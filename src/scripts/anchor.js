0b45182cfdab6c89fcfe314acd09d31611aa688e
Сергей Викторович Князев <113102209+Knyazew36@users.noreply.github.com>
2023-07-21 17:43:02 +0300

    q

--- src/scripts/anchor.js
+++ src/scripts/anchor.js
@@ -1,52 +1,30 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Получаем все ссылки с атрибутом href, начинающимся с #
-  const anchorLinks = document.querySelectorAll('a[href^="#"]');
-
-  // Получаем значение из localStorage для расстояния прокрутки
-  let scrollOffsetValue = parseInt(localStorage.getItem("scrollOffsetValue")) || 200;
-
-  // Обрабатываем клик по каждой якорной ссылке
-  anchorLinks.forEach(link => {
-    link.addEventListener("click", function (event) {
-      // Отменяем стандартное поведение ссылки
-      event.preventDefault();
-
-      // Получаем элемент, к которому ведет якорь
-      const targetElement = document.querySelector(link.getAttribute("href"));
-
-      // Если элемент найден, выполняем прокрутку к нему
-      if (targetElement) {
-        // Вычисляем позицию элемента относительно верхней границы документа
-        const targetPosition = targetElement.getBoundingClientRect().top;
-
-        // Вычисляем позицию элемента относительно верхней границы окна просмотра
-        const offsetPosition = targetPosition + window.pageYOffset;
-
-        // Выполняем плавную прокрутку к элементу с учетом отступа
-        window.scroll({
-          top: offsetPosition - scrollOffsetValue,
-          behavior: "smooth"
-        });
-      }
-    });
-  });
-
-  // При загрузке страницы, учитываем якорь, если он присутствует в URL
-  const hash = window.location.hash;
-  if (hash) {
-    const targetElement = document.querySelector(hash);
-    if (targetElement) {
-      const targetPosition = targetElement.getBoundingClientRect().top;
-      const offsetPosition = targetPosition + window.pageYOffset;
-      window.scroll({
-        top: offsetPosition - scrollOffsetValue,
-        behavior: "smooth"
-      });
+$(document).ready(function () {
+  function scrollToElementWithOffset(targetElement) {
+    if (targetElement.length) {
+      const offset = targetElement.offset().top - 130;
+      $('html, body').animate(
+        {
+          scrollTop: offset,
+        },
+        500
+      );
     }
   }
 
-  // Сохраняем значение scrollOffsetValue в localStorage при изменении
-  window.addEventListener("beforeunload", function () {
-    localStorage.setItem("scrollOffsetValue", scrollOffsetValue.toString());
-  });
+  function handleHashLinkClick(event) {
+    event.preventDefault();
+
+    const target = $(this).attr('href');
+    const targetElement = $(target);
+
+    scrollToElementWithOffset(targetElement);
+  }
+
+  const hash = window.location.hash;
+  if (hash) {
+    const targetElement = $(hash);
+    scrollToElementWithOffset(targetElement);
+  }
+
+  $('a[href^="#"]').on('click', handleHashLinkClick);
 });
