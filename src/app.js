498744f1d1f10559b12ca28283e8b154b25070c7
Сергей Викторович Князев <113102209+Knyazew36@users.noreply.github.com>
2023-07-24 18:14:27 +0300

    1

--- src/app.js
+++ src/app.js
@@ -6,14 +6,12 @@ import './scripts/anchor';
 import './scripts/preloader';
 import './scripts/mediaQueries';
 import './scripts/intersection';
-import './scripts/aos';
 import './scripts/countup';
 import './scripts/print-button';
 import './scripts/form-components';
 import { fancybox } from './scripts/fancybox';
 import { modal } from './scripts/modal';
-import 'lazysizes';
-import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+
 document.addEventListener('DOMContentLoaded', () => {
   fancybox();
   modal();
